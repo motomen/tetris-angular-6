de364b48b9c57e1449e28cf2d0565df614d25890
Yaroslav <delphi7c@gmail.com>
2018-08-15 09:13:50 +0300

    test(game board): filled array

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { GameBoardComponent } from './game-board/game-board.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        GameBoardComponent,
       ],
     }).compileComponents();
   }));
--- src/app/game-board/game-board.component.spec.ts
+++ src/app/game-board/game-board.component.spec.ts
@@ -22,4 +22,39 @@ describe('GameBoardComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  describe('fillArray', () => {
+    it('should fill array 0 by default', () => {
+      // given
+      const expectData = 0;
+      // when
+      const result = component.fillArray();
+      // then
+      expect(result[0]).toEqual(expectData);
+    });
+
+    it('should fill array 41 element by default', () => {
+      // given
+      const expectData = 41;
+      // when
+      const result = component.fillArray();
+      // then
+      expect(result.length).toEqual(expectData);
+    });
+    it('should fill array with 1', () => {
+      // given
+      const expectData = 1;
+      // when
+      const result = component.fillArray(1);
+      // then
+      expect(result[0]).toEqual(expectData);
+    });
+    it('should fill array with 4 size', () => {
+      // given
+      const expectData = [1, 1, 1, 1];
+      // when
+      const result = component.fillArray(1, 4);
+      // then
+      expect(result).toEqual(expectData);
+    });
+  });
 });
